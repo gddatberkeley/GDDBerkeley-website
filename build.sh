1faa533a5963653cb74cc38bbbace3cdd6d73b21
johnny <joeywsd@gmail.com>
2026-02-08 14:15:41 -0800

    change build.sh to check  for git changes so that the cronjob checks for gdrive assets change and git change so I don't have to ssh in all the time

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 PATH=$PATH:~/bin
-
+#github check
+echo "github pull"
+git pull origin main
+#google drive check even though not sure if google drive exists/who has access
 if rclone check gdd-gdrive:website-assets assets/gdrive; then
     echo "(¬‿¬) No differences detected."
 else
     # File are different
     echo "Found changes, rebuilding website"
     rclone sync gdd-gdrive:website-assets assets/gdrive
-    hugo -d ~/public_html
 fi
+#build website
+hugo -d ~/public_html
